bc8255e4503387e29d38a236a23ba1ed52d0f467
Edwin Mah <edwinmah@users.noreply.github.com>
2017-08-01 15:36:54 -0400

    Remove Access-Control-Allow-Origin header

--- js/app.js
+++ js/app.js
@@ -17,7 +17,6 @@ $.ajax({
   url: 'https://api.propublica.org/campaign-finance/v1/2016/president/totals.json',
   dataType: "json",
   type: "GET",
-  headers: {'Access-Control-Allow-Origin': '*'},
   beforeSend: function(xhr) {
     xhr.setRequestHeader('X-API-Key', 'tqIQ49k4JY7c2IczBaWGO5ApuGboqHhz6MtDhV6N')
   }
